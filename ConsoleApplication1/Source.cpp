5de1102fd35e72200b6fa4feb7c617c2e4f88a32
Thalente Innocent Ngcobo <mrti.ngcobo@yahoo.com>
2016-03-08 10:24:20 +0200

    added comments

---
+++ ConsoleApplication1/Source.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+//inheritance example
+
+class Animal{
+
+protected:
+	string name;
+public:
+	Animal(){
+		name = "Animal";
+	}
+	void whoAmi(void){
+		cout << "I am "<< name << endl;
+
+	}
+	virtual void does(void){
+		cout << "do nothing" << endl;
+	}
+
+};
+
+class Dog :public Animal{
+protected :
+	int numberLegs;
+public:
+	Dog()
+	{
+		name = "Dog";
+		numberLegs = 4;
+	}
+	void does(void)
+	{
+		cout << "Woof!" << endl;
+	}
+	int howManyLegs(){
+		return numberLegs;
+	}
+};
+//main class 
+int main()
+{
+	Animal myAnimal;
+	Dog myDog;
+
+	Animal *animalPtr;
+
+	animalPtr = &myDog;
+
+
+	myAnimal.whoAmi();
+	myAnimal.does();
+
+    myDog.whoAmi();
+	myDog.does();
+
+	cout << "Dogs have " << myDog.howManyLegs()<< " legs." <<endl;
+	cout << "Using base class pointer from the derieved class " << endl;
+	animalPtr->whoAmi();
+	animalPtr->does();
+
+
+}
